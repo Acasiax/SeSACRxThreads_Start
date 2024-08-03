340f36c6aac452f88ee543bf2cafabe7a537e1f6
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-08-03 23:53:15 +0900

    버튼의 위치 설정에 SnapKit을 사용하지 않고, 버튼의 크기만 설정하여 테이블 뷰 셀의 기본 accessoryView 위치를 따르도록

--- SeSACRxThreads_Start/BucketListTableVC.swift
+++ SeSACRxThreads_Start/BucketListTableVC.swift
@@ -17,9 +17,13 @@ struct ShoppingItem {
 }
 
 class SimpleShoppingListViewController: UIViewController, UITableViewDelegate {
+    // UI 컴포넌트
     var tableView: UITableView!
+    
+    // RxSwift Dispose Bag
     let disposeBag = DisposeBag()
     
+    // 쇼핑 아이템
     let items = BehaviorRelay<[ShoppingItem]>(value: [
         ShoppingItem(title: "그림툭 구매하기", isChecked: true, isFavorite: true),
         ShoppingItem(title: "사이다 구매", isChecked: true, isFavorite: false),
@@ -27,13 +31,26 @@ class SimpleShoppingListViewController: UIViewController, UITableViewDelegate {
         ShoppingItem(title: "양말", isChecked: true, isFavorite: true)
     ])
     
+    // MARK: - 뷰 디드로드
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupView()
+        setupTableView()
+        bindTableViewData()
+        handleTableViewSelection()
+    }
+    
+    // MARK: - 뷰 설정
+    
+    private func setupView() {
         self.title = "쇼핑"
         view.backgroundColor = .white
-        
-        // 테이블 뷰 설정
+    }
+    
+    // MARK: - 테이블 뷰 설정
+    
+    private func setupTableView() {
         tableView = UITableView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         view.addSubview(tableView)
@@ -41,28 +58,39 @@ class SimpleShoppingListViewController: UIViewController, UITableViewDelegate {
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
-        // 데이터 바인딩
-        items.bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, item, cell) in
+    }
+    
+    // MARK: - 데이터 바인딩
+    
+    private func bindTableViewData() {
+        items.bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { [weak self] (row, item, cell) in
+            guard let self = self else { return }
             cell.textLabel?.text = item.title
             cell.accessoryView = self.createAccessoryView(isFavorite: item.isFavorite, index: row)
         }
         .disposed(by: disposeBag)
-        
-        //셀 선택했을때
-        tableView.rx.itemSelected
-            .subscribe(onNext:  { indexPath in
-                print("선택했어 \(indexPath.row)")
-            })
-            .disposed(by: disposeBag)
-        
     }
     
-    func createAccessoryView(isFavorite: Bool, index: Int) -> UIView {
+    // MARK: - 선택 처리
+    
+    private func handleTableViewSelection() {
+        tableView.rx.itemSelected
+            .subscribe(onNext: { [weak self] indexPath in
+                self?.toggleChecked(at: indexPath.row)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: - 액세서리 뷰
+    
+    private func createAccessoryView(isFavorite: Bool, index: Int) -> UIView {
         let button = UIButton(type: .system)
         let image = isFavorite ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
         button.setImage(image, for: .normal)
         button.tag = index
+        
+        // 버튼의 크기 설정
+        button.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
         
         // Rx를 사용하여 버튼의 터치 이벤트 처리
         button.rx.tap
@@ -71,15 +99,22 @@ class SimpleShoppingListViewController: UIViewController, UITableViewDelegate {
             }
             .disposed(by: disposeBag)
         
-        
         return button
-        
     }
     
-    func toggleFavorite(at index: Int) {
-            var currentItems = items.value
-            currentItems[index].isFavorite.toggle()
-            items.accept(currentItems)
-        }
+    // MARK: - 즐겨찾기 토글
     
+    private func toggleFavorite(at index: Int) {
+        var currentItems = items.value
+        currentItems[index].isFavorite.toggle()
+        items.accept(currentItems)
+    }
+
+    // MARK: - 체크 토글
+
+    private func toggleChecked(at index: Int) {
+        var currentItems = items.value
+        currentItems[index].isChecked.toggle()
+        items.accept(currentItems)
+    }
 }
