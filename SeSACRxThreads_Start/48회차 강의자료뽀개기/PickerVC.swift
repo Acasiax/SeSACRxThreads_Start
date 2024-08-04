6e9adb345a554dc15ceb3d9882bfd321e2d99680
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-08-04 20:41:30 +0900

    데이터피커 구현 연습

--- SeSACRxThreads_Start/48회차 강의자료뽀개기/PickerVC.swift
+++ SeSACRxThreads_Start/48회차 강의자료뽀개기/PickerVC.swift
@@ -26,20 +26,21 @@ class PickerVC: UIViewController {
         pickerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
+        //내가 만든 아이템을 rx를 통하여 데이터피커에 넣는 과정
         items
             .bind(to: pickerView.rx.itemTitles) { (row, element) in
-                return element
+                return element // 각 행의 타이틀로 element 반환
             }
             .disposed(by: disposeBag)
+          
         
-        
-        pickerView.rx.modelSelected(String.self)
-            .subscribe(onNext: {models in
-            print("선택됨: \(models)")
+        // modelSelected(String.self)는 UIPickerView에서 특정 타입(String)의 모델이 선택되었을 때 이를 Observable로 만들어주는 메서드입니다.
+        // 즉, 사용자가 UIPickerView에서 아이템을 선택할 때마다 이 Observable이 이벤트를 방출하게 됩니다.
+        pickerView.rx.modelSelected(String.self) // 선택된 모델을 관찰
+            .subscribe(onNext: {items in // 선택된 모델을 구독
+            print("선택됨: \(items)")
             })
             .disposed(by: disposeBag)
-        
     }
     
 }
