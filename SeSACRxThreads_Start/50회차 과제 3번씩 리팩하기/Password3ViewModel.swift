b819e091b05c5244873751df96687b51c1d31742
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-08-06 00:23:13 +0900

    뷰모델에 uikit 제거, 컬러 제거하여 다시 구성함

--- SeSACRxThreads_Start/50회차 과제 3번씩 리팩하기/Password3ViewModel.swift
+++ SeSACRxThreads_Start/50회차 과제 3번씩 리팩하기/Password3ViewModel.swift
@@ -5,39 +5,38 @@
 //  Created by 이윤지 on 8/5/24.
 //
 
-import UIKit
+import Foundation
 import RxSwift
 import RxCocoa
 
 class Password3ViewModel {
     
     struct Input {
-        let passwordText: Driver<String>
+        let password: Driver<String>
         let tap: Driver<Void>
     }
     
     struct Output {
         let validText: Driver<String>
         let isValid: Driver<Bool>
-        let isDescriptionHidden: Driver<Bool>
-        let nextButtonColor: Driver<UIColor>
+        let descriptionHidden: Driver<Bool>
         let showAlert: Driver<Void>
     }
     
     func transform(input: Input) -> Output {
-        let validation = input.passwordText.map { $0.count >= 8 }
+        let validation = input.password.map { $0.count >= 8 }
         
         let validText = Driver.just("8자 이상 입력해주세요")
         
-        let isDescriptionHidden = validation.map { !$0 }
-        
-        let nextButtonColor = validation.map {$0 ? UIColor.systemPink : UIColor.lightGray}
+        let descriptionHidden = validation.map { $0 }
         
         let showAlert = input.tap
         
-        return Output(validText: validText, isValid: validation, isDescriptionHidden: isDescriptionHidden, nextButtonColor: nextButtonColor, showAlert: showAlert
+        return Output(
+            validText: validText,
+            isValid: validation,
+            descriptionHidden: descriptionHidden,
+            showAlert: showAlert
         )
-        
-        
     }
 }
